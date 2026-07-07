300a3c75d74dec600164f2beb4b9215c3022861c
swathieswaramoorthy <swathieswaramoorthy2005@gmail.com>
2026-07-07 20:33:14 +0530

    backend api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,26 +1,26 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//     baseURL: "https://product-crud-1-41wv.onrender.com/api"
-// });
-
-// export default API;
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://product-crud-1-41wv.onrender.com/api",
-// baseURL:"http://localhost:5000/api",
-});
-
-// Attach JWT token automatically
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+    baseURL: "https://product-crud-1-41wv.onrender.com/api"
 });
 
 export default API;
+// import axios from "axios";
+
+// const API = axios.create({
+//  // baseURL: "https://product-crud-1-41wv.onrender.com/api",
+// // baseURL:"http://localhost:5000/api",
+// });
+
+// // Attach JWT token automatically
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+// export default API;
